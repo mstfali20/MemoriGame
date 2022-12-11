61648d465a4033e4c7c9b795852689ac2de795aa
Mustafa Ali KILCI <mustafa_klc997@hotmail.com>
2022-12-11 14:39:05 +0300

    Initial Commit

---
+++ MemoriGame/MemoriGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriGameApp.swift
+//  MemoriGame
+//
+//  Created by Mustafa Ali KILCI on 11.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemoriGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
